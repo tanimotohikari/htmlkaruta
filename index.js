7556751d4199c2605be20e66f96bccc774bab2a8
sisiQ <hikaritanimoto@hikaritanimoto-no-iMac.local>
2017-08-18 07:16:47 +0900

    処理の変更

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ io.on('connection', function(socket) {
   //かるたがクリックされた時の処理
   socket.on('emitFromClient', function(data) {
     console.log(data);
-    sio.sockets.in(channel).emit('emitFromServer', data);
+    io.sockets.in(channel).emit('emitFromServer', data);
     socket.broadcast.to.(channel).emit('emitFromServer', data);
   });
 });
